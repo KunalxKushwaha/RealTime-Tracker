95f48e7395c23057a18614189d9510005fc330f7
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-08-03 23:08:34 +0530

    Updation

--- app.js
+++ app.js
@@ -10,9 +10,20 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view Engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views")); // your 'views' folder path
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function(socket) {
+    socket.on('send-location', (data) => {
+        io.emit('recieve-location', {id : socket.id, ...data});
+    });
+    socket.on('disconnect', () => {
+        io.emit('user-disconnects', {id : socket.id, latitude: null, longitude: null});
+    });
+});
 
 app.get("/", function(req, res) {
-    res.send("Hello");
+    res.render("index.ejs");
 });
 server.listen(3000);
